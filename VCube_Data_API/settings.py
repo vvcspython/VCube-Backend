8c5e3f9cb93d9a7b0ce71870ba8c211b1c66a2a1
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-09-14 17:52:38 +0530

    Update settings.py

--- VCube_Data_API/settings.py
+++ VCube_Data_API/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = [
     'localhost',
     'vcube-backend.onrender.com',
     '.vercel.app',
+    '.now.sh',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -182,11 +183,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
