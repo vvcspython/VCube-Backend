011281767d4083b086ad01803916ab6ce0f2f20a
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-09-14 13:52:14 +0530

    Update settings.py

--- VCube_Data_API/settings.py
+++ VCube_Data_API/settings.py
@@ -37,20 +37,21 @@ X_FRAME_OPTIONS = 'DENY'
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 
-    'localhost'
+    'localhost',
+    'vcube-backend.onrender.com',
 ]
-
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
+    'https://vcube-backend.onrender.com',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
+    'https://vcube-backend.onrender.com',
 ]
-
 
 # Application definition
 
