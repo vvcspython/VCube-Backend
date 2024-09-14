7a53227ee322a546732b826c1517b68411af5df2
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-09-14 15:03:35 +0530

    Update models.py

--- Data_API/models.py
+++ Data_API/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
-from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
