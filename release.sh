1e272e1cace9f3bb3e1475f979a5c7d26759636a
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-09-15 22:14:34 +0530

    Create release.sh

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Run database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the Gunicorn server
+exec gunicorn VCube_Data_API.wsgi:application --bind 0.0.0.0:8000
