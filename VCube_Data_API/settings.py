a092224caf0e74e2dc185def5f419fd7f7bb53b8
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-09-15 23:17:19 +0530

    Update settings.py

--- VCube_Data_API/settings.py
+++ VCube_Data_API/settings.py
@@ -39,22 +39,21 @@ ALLOWED_HOSTS = [
     '127.0.0.1', 
     'localhost',
     'vcube-backend.onrender.com',
-    '.vercel.app',
-    '.now.sh',
+    'vcube-backend-1.onrender.com',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
     'https://vcube-backend.onrender.com',
-    'https://*.vercel.app',
+    'https://vcube-backend-1.onrender.com',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
     'https://vcube-backend.onrender.com',
-    'https://*.vercel.app',
+    'https://vcube-backend-1.onrender.com',
 ]
 
 # Application definition
