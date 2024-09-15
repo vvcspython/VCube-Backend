13a2c21e33c0ee8dd9396057bc3c0f55b85e00b9
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-09-15 23:47:22 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Apply database migrations
-/home/myuser/venv/bin/python manage.py makemigrations
-/home/myuser/venv/bin/python manage.py migrate
+/code/venv/bin/python manage.py makemigrations
+/code/venv/bin/python manage.py migrate
 
 # Collect static files (optional, if you have static files)
-# /home/myuser/venv/bin/python manage.py collectstatic --noinput
+# /code/venv/bin/python manage.py collectstatic --noinput
 
 # Start Gunicorn server
-exec /home/myuser/venv/bin/gunicorn --bind 0.0.0.0:8000 VCube_Data_API.wsgi:application
+exec /code/venv/bin/gunicorn --bind 0.0.0.0:8000 VCube_Data_API.wsgi:application
