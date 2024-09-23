1f7853b836a4dc49f9122fcb24bc756702167a66
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-09-23 17:21:33 +0530

    Update managing_data_view.py

--- Data_API/managing_data_view.py
+++ Data_API/managing_data_view.py
@@ -24,7 +24,8 @@ from .models import (
     ClassRecordingsData,
     StudentWatchTimeData,
     PermissionsData,
-    FeedbackFormLists
+    FeedbackFormLists,
+    ReportData,
 )
 from .serializers import (
     BatchDataSerializer,
@@ -43,7 +44,8 @@ from .serializers import (
     ClassRecordingsDataSerializer,
     StudentWatchTimeDataSerializer,
     PermissionsDataSerializer,
-    FeedbackFormListsSerializer
+    FeedbackFormListsSerializer,
+    ReportDataSerializer,
 )
 
 
@@ -68,7 +70,8 @@ class ManagingDataView(APIView):
         'class_recordings' : (ClassRecordingsData, ClassRecordingsDataSerializer),
         'student_watchtime' : (StudentWatchTimeData, StudentWatchTimeDataSerializer),
         'permissions_data' : (PermissionsData, PermissionsDataSerializer),
-        'feedbackforms-list' : (FeedbackFormLists, FeedbackFormListsSerializer)
+        'feedbackforms-list' : (FeedbackFormLists, FeedbackFormListsSerializer),
+        'report-data' : (ReportData, ReportDataSerializer),
     }
 
     def get_model_and_serializer(self, model_name):
@@ -111,6 +114,12 @@ class ManagingDataView(APIView):
         serializer = serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            
+            if model_name == 'batch_data':
+                res = addBatchPermissions(request.data)
+                if not res:
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -167,3 +176,122 @@ class ManagingDataView(APIView):
         
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@method_decorator(token_required, name='dispatch')
+class ManageStudentData(APIView):
+    
+    def post(self, request):
+        data = request.data
+        if not isinstance(data, list):
+            return Response({'error': 'Data must be a list of students'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = StudentDataSerializer(data=data, many=True)
+        if serializer.is_valid():
+            students = [StudentData(**item) for item in serializer.validated_data]
+            StudentData.objects.bulk_create(students)
+            return Response({'status': 'Students uploaded successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        course = request.data.get('Course')
+        batch = request.data.get('BatchName')
+        
+        if not course or not batch:
+            return Response({'error': 'Course and BatchName are required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        students = StudentData.objects.filter(Course=course, BatchName=batch)
+        stdAtt = StudentAttendance.objects.filter(Course=course, BatchName=batch)
+        stdWatchTime = StudentWatchTimeData.objects.filter(Course=course, BatchName=batch)
+        stdMsg = StudentMessagesData.objects.filter(Course=course, BatchName=batch)
+        
+        if students.exists():
+            deleted_count, _ = students.delete()
+            if stdAtt.exists():
+                stdAtt.delete()
+            if stdWatchTime.exists():
+                stdWatchTime.delete()
+            if stdMsg.exists():
+                stdMsg.delete()
+        else:
+            return Response({'status' : 'Students data not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response({'status': f'{deleted_count} students deleted.'}, status=status.HTTP_204_NO_CONTENT)
+    
+
+@method_decorator(token_required, name='dispatch')
+class ManageStdAttandanceData(APIView):
+    
+    def delete(self, request, id):
+
+        if not id:
+            return Response({'error': 'id required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        stdData = StudentAttendance.objects.filter(StudentId=id)
+        stdWatchTime = StudentWatchTimeData.objects.filter(StudentId=id)
+        stdMsg = StudentMessagesData.objects.filter(StudentId=id)
+        std_Msgs = BatchToStudentMessagesData.objects.filter(StudentId=id)
+        
+        if stdData.exists():
+            stdData.delete()
+        if stdWatchTime.exists():
+            stdWatchTime.delete()
+        if stdMsg.exists():
+            stdMsg.delete()
+        if std_Msgs.exists():
+            std_Msgs.delete()
+            
+        return Response({'status': 'Students attandance data deleted.'}, status=status.HTTP_204_NO_CONTENT)
+    
+@method_decorator(token_required, name='dispatch')
+class ManageBatchAttandanceData(APIView):
+    
+    def delete(self, request, id):
+
+        if not id:
+            return Response({'error': 'id required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        batchData = BatchAttendance.objects.filter(BatchID=id)
+        
+        if not batchData.exists():
+                return Response({'status': 'No records found for the provided id.'}, status=status.HTTP_404_NOT_FOUND)
+            
+        get_data = batchData.first()
+        
+        batchMsg = BatchMessagesData.objects.filter(BatchID=id)
+        batch_Msgs = BatchToStudentMessagesData.objects.filter(BatchID=id)
+        batch_std_Msgs = BatchToStudentMessagesData.objects.filter(Course=get_data.Course, BatchName=get_data.BatchName)
+        batch_permissions = PermissionsData.objects.filter(Course=get_data.Course, BatchName=get_data.BatchName)
+        
+        batchData.delete()
+        if batchMsg.exists():
+            batchMsg.delete()
+        if batch_Msgs.exists():
+            batch_Msgs.delete()
+        if batch_std_Msgs.exists():
+            batch_std_Msgs.delete()
+        if batch_permissions.exists():
+            batch_permissions.delete()
+            
+        return Response({'status': 'Students attandance data deleted.'}, status=status.HTTP_204_NO_CONTENT)
+    
+def addBatchPermissions(data):
+    try:
+        batchData = BatchData.objects.get(Course=data.get('Course'), BatchName=data.get('BatchName'))
+    except BatchData.DoesNotExist:
+        return False
+
+    permission = {
+        "BatchID": batchData.id,
+        "Course": batchData.Course,
+        "BatchName": batchData.BatchName
+    }
+
+    serializer = PermissionsDataSerializer(data=permission)
+    if serializer.is_valid():
+        serializer.save()
+        return True
+    else:
+        print(serializer.errors)
+        batchData.delete()
+        return False
